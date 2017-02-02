fbd6541161a2c79ed5c8f47abf7cd0a9ca51428a
istepgueu <stephane.gueulette@gmail.com>
2017-02-02 22:42:14 +0100

    Update MMM-door-sensor.js

--- MMM-door-sensor.js
+++ MMM-door-sensor.js
@@ -10,14 +10,51 @@
 Module.register("MMM-door-sensor",{
 
 	// Default module config.
-	defaults: {
-		text: "Hello World!"
+	//defaults: {
+	//	text: "Hello World!"
+	//},
+
+
+	// Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
 	},
 
-	// Override dom generator.
+
+
+	notificationReceived: function(notification, payload, sender) {
+		if (sender) {
+			Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+			if (notification === "REMOTE_ACTION") {
+				this.sendSocketNotification(notification, payload);	
+			}
+		} else { 
+			if (notification === "DOM_OBJECTS_CREATED") {
+				this.sendSocketNotification("REQUEST_DEFAULT_SETTINGS");
+			}
+		}
+	},
+
+
+
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+
+		if (notification === "SHOW_ALERT") {
+                      wrapper.innerHTML = "TOTO";
+		}
+
+		} else {
+
+                if (notification === "HIDE_ALERT") {
+			wrapper.innerHTML = "TATA";
+		}
+
 		return wrapper;
-	}
+	},
+
+
+
+
 });
