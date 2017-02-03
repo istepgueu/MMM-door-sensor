5bad4676e6ea8bc46fa2f85329dde4f0f866c090
istepgueu <stephane.gueulette@gmail.com>
2017-02-03 17:26:14 +0100

    Update MMM-door-sensor.js

--- MMM-door-sensor.js
+++ MMM-door-sensor.js
@@ -22,17 +22,37 @@ Module.register("MMM-door-sensor",{
         },
 
 
-
-                notificationReceived: function(notification, payload, sender) {
+		notificationReceived: function(notification, payload, sender) {
                 if (sender) {
                         Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-                        if (notification === "DOOR_STATE") {
 
-                                this.door_state = payload;
 
-                                this.updateDom();
+			if (notification === "DOOR_CLOSE_avant") {
+						this.door_state = "door_1_close.png";
+                                		 this.updateDom();   						 
 
+			} else if (notification === "DOOR_OPEN_avant") {
+						this.door_state = "door_1_open.png";
+                                		 this.updateDom();
+
+			} else {
+			}
+
+
+			if (notification === "DOOR_CLOSE_arriere") {
+                                                this.door_state2 = "door_2_close.png";
+                                                 this.updateDom();
+
+                        } else if (notification === "DOOR_OPEN_arriere") {
+                                                this.door_state2 = "door_2_open.png";
+                                                 this.updateDom();
+
+                        } else {
                         }
+
+
+
+
                 }
 
         },
@@ -42,11 +62,15 @@ Module.register("MMM-door-sensor",{
                 getDom: function() {
 
                         var wrapper = document.createElement("div");
-                        if(this.door_state){
-                        wrapper.innerHTML = "<img src=/modules/MMM-door-sensor/img/"+this.door_state+" />";
-                        } else {
-                        wrapper.innerHTML = 'No door state present.';
-                        }
-                        return wrapper;
+                        if(this.door_state && this.door_state2){
+                        wrapper.innerHTML = "<img src='/modules/MMM-door-sensor/img/"+this.door_state+"' width='100' />";
+			wrapper.innerHTML += "<img src='/modules/MMM-door-sensor/img/"+this.door_state2+"' width='100' />";  
+			} else {
+
+			wrapper.innerHTML = "<img src='/modules/MMM-door-sensor/img/door_1_blanc.png' width='100' />";
+                        wrapper.innerHTML += "<img src='/modules/MMM-door-sensor/img/door_2_blanc.png' width='100' />";
+			}
+                      return wrapper;
+//		    }
                 }
 });
