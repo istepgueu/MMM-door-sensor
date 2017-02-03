76972aa13313da57dc221e3997a03fde3bc0863f
istepgueu <stephane.gueulette@gmail.com>
2017-02-03 09:01:21 +0100

    Update MMM-door-sensor.js

--- MMM-door-sensor.js
+++ MMM-door-sensor.js
@@ -9,55 +9,44 @@
 
 Module.register("MMM-door-sensor",{
 
-	// Default module config.
-	defaults: {
-		text: "Hello World!"
-	},
+        // Default module config.
+        defaults: {
+                text: "Hello World!"
+        },
 
 
-	// Define start sequence.
-	start: function() {
-		Log.info("Starting module: " + this.name);
+        // Define start sequence.
+        start: function() {
+                Log.info("Starting module: " + this.name);
 
-	},
-
-
-
-	notificationReceived: function(notification, payload, sender) {
-		if (sender) {
-			Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-			if (notification === "REMOTE_ACTION") {
-				this.sendSocketNotification(notification, payload);	
-
-				var door_state = "close.png";			
-	
-				this.updateDom();				
-			
-			}
-		} else { 
-			if (notification === "DOM_OBJECTS_CREATED") {
-				this.sendSocketNotification("REQUEST_DEFAULT_SETTINGS");
-			
-				var door_state = "open.png";
-
-
-				this.updateDom();
-
-
-			}
-		}
-		
-		},
-
-
-		getDom: function() {
-
-		var wrapper = document.createElement("div");
-		return door_state;
-		wrapper.innerHTML = '<img id="door_state" src=' + door_state + ' width="100" height="200" />';
-		return wrapper;
-	}
+        },
 
 
 
+                notificationReceived: function(notification, payload, sender) {
+                if (sender) {
+                        Log.log(this.name + " received a module notification: " + notification + " from se$
+                        if (notification === "DOOR_STATE") {
+
+                                this.door_state = payload;
+
+                                this.updateDom();
+
+                        }
+                }
+
+        },
+
+
+
+                getDom: function() {
+
+                        var wrapper = document.createElement("div");
+                        if(this.door_state){
+                        wrapper.innerHTML = "<img src=/modules/MMM-door-sensor/img/"+this.door_state+" />";
+                        } else {
+                        wrapper.innerHTML = 'No door state present.';
+                        }
+                        return wrapper;
+                }
 });
